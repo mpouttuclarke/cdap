a3789869ee08fc83e10ff4c5745859c21830cd29
Jiahua Wang <jiahuawang7@gmail.com>
2015-11-05 14:41:35 -0800

    CDAP-4180: Caching for lookup
    - Add 'cache' property to LookupTableConfig
    - If 'cache' property is present, caching is enabled, and ScriptLookup
      wraps its Lookup delegate in a CachingLookup instance
    - CachingLookup uses Guava's LoadingCache/CacheBuilder to provide
      caching mechanisms. It uses expireAfterWrite and maximumSize. Both are
      configurable using the 'cache' property.

--- cdap-app-templates/cdap-etl/cdap-etl-api/src/main/java/co/cask/cdap/etl/api/LookupTableConfig.java
+++ cdap-app-templates/cdap-etl/cdap-etl-api/src/main/java/co/cask/cdap/etl/api/LookupTableConfig.java
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package co.cask.cdap.etl.api;
 
 import com.google.common.base.Preconditions;
@@ -34,11 +35,22 @@ public class LookupTableConfig {
   }
 
   private final TableType type;
+
+  @Nullable
   private final Map<String, String> datasetProperties;
 
-  public LookupTableConfig(TableType type, @Nullable Map<String, String> datasetProperties) {
+  @Nullable
+  private final CacheConfig cache;
+
+  public LookupTableConfig(TableType type, @Nullable CacheConfig cache,
+                           @Nullable Map<String, String> datasetProperties) {
     this.type = type;
+    this.cache = cache;
     this.datasetProperties = datasetProperties;
+  }
+
+  public LookupTableConfig(TableType type) {
+    this(type, null, null);
   }
 
   public TableType getType() {
@@ -48,5 +60,13 @@ public class LookupTableConfig {
   public Map<String, String> getDatasetProperties() {
     Preconditions.checkArgument(type == TableType.DATASET);
     return datasetProperties == null ? ImmutableMap.<String, String>of() : datasetProperties;
+  }
+
+  public boolean isCacheEnabled() {
+    return cache != null;
+  }
+
+  public CacheConfig getCacheConfig() {
+    return Preconditions.checkNotNull(cache);
   }
 }
